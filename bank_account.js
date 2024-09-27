56ff9859e92b7e4601ff29f87692a1bc1493a009
naufalnak <naufal.ndak17@gmail.com>
2024-09-27 21:42:12 +0700

    feat: add withdraw

--- bank_account.js
+++ bank_account.js
@@ -12,6 +12,17 @@ class BankAccount {
       throw new Error("Jumlah deposit harus lebih dari 0.");
     }
   }
+
+  withdraw(jumlah) {
+    if (jumlah > 0 && jumlah <= this.balance) {
+      this.balance -= jumlah;
+      return this.balance;
+    } else if (jumlah > this.balance) {
+      throw new Error("Saldo tidak mencukupi.");
+    } else {
+      throw new Error("Jumlah penarikan harus lebih dari 0.");
+    }
+  }
 }
 
 module.exports = BankAccount;
